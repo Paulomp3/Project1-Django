5981ff3cbd470dd5aad849ee1bf7aecee4965452
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-02-25 11:06:26 -0300

    fix: correções de views

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def gambiarra(request):
-    return render(request, 'gambiarra.html')
+    return render(request, 'recipes/gambiarra.html')
 def raiz(request):
-    return render(request, 'gambiarra2.html')
+    return render(request, 'global/gambiarra2.html')
 def pagina2 (request):
     return HttpResponse('pagina 2 do meu site')
 
