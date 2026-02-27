14a3ccf083a6e42f1bf94d4cbc60fcad4d9ce542
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-02-27 11:27:22 -0300

    começo do projeto

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from recipes.views import pagina3,pagina1,pagina2 
+from recipes.views import pagina1
 
 urlpatterns = [
 
-    path('pagina3/', pagina3),
     path('', pagina1 ),
-    path('pagina2/',pagina2)
+    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,16 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def pagina3(request):
-    return render(request, 'recipes/pagina3.html', context= {
-        'nome': 'denise silva de carvalho vasconcelos '
-    })
 def pagina1(request):
     dados = 'eu sou muito lindo slk'
     context = {'chave': dados}
     return render(request, 'recipes/pagina1.html',context)
-def pagina2 (request):
-    return render(request,'recipes/pagina2.html', context = {
-        'nome': 'paulo silva de carvalho vasconcelos'
-    })
+
 
