59ac4bfab67babd57f229b15680d89dd07964251
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-03-02 22:33:06 -0300

    feat: criação de urls com id, e correção da janela de view e home

--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,12 @@ def home(request):
     dados = 'eu sou muito lindo slk'
     context = {'chave': dados}
     return render(request, 'recipes/pages/home.html',context)
-
+def recipes(request):
+    dados = 'eu sou muito lindo slk'
+    context = {'chave': dados}
+    return render(request, 'recipes/pages/home.html',context)
+def recipes2(request, id):
+    dados = 'eu sou muito lindo slk'
+    context = {'chave': dados}
+    return render(request,'recipes/pages/recipe-view.html',context)
 
