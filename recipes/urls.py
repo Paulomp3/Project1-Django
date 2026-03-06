a08ad91c82a62a5c4dc5528c0410b49f748e3b4d
Paulo silva de Carvalho Vasconcelos <paulosvasconcelos99@outlook.com>
2026-03-06 10:44:24 -0300

    fix: correções de urls para padrão moderno, usando o name = dentro da view e fazendo os links especificos

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/',views.recipes),
-    path('recipes/<id>/',views.recipes2),
+    path('', views.home, name='recipes-home'),
+    path('recipes/',views.recipes, name='recipes-list'),
+    path('recipes/<id>/',views.recipes2, name='recipes-detail'),
 ]
